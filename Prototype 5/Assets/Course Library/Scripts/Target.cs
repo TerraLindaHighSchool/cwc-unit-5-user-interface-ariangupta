55ef006361305d35b35c86bdac5d2a7215f3601c
ariangupta <59099013+ariangupta@users.noreply.github.com>
2020-03-12 14:16:59 -0700

    5.2
    added particle effects and a score

--- Prototype 5/Assets/Course Library/Scripts/Target.cs
+++ Prototype 5/Assets/Course Library/Scripts/Target.cs
@@ -7,6 +7,10 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
         targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
         transform.position = new Vector3(Random.Range(-4, 4), -6);
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -25,6 +30,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(pointValue);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
     }
 
     private void OnTriggerEnter(Collider other)
